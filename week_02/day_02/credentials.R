ba0c3a5930c0231025de30fd843e346e45a37add
cproudfoot92 <50339446+cproudfoot92@users.noreply.github.com>
2020-07-08 08:46:15 +0100

    homework _w2_d2

---
+++ week_02/day_02/credentials.R
@@ -0,0 +1,10 @@
+postgresql_username <- "christopherproudfoot"
+postgresql_password <- "T!nker123"
+
+remote_omni_postgresql_username <- "cjitjeca"
+remote_omni_postgresql_password <- "dt1SkrwzsAMBGQnyKaNG-nSKFoo9snlP" 
+
+username <- 'mvhbdgdk'
+password <- 'Ejn5vEqOUfCA1Cxg1rqkh9NY5VbKKAIm'
+database_name <- 'mvhbdgdk'
+host <- 'manny.db.elephantsql.com' 
